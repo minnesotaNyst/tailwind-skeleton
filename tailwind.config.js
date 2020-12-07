dba859ee640b832c664d3518fff63f59822f2524
minnesotaNyst <71350960+minnesotaNyst@users.noreply.github.com>
2020-12-06 20:38:54 -0600

    trying to make changes to figure out tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
 	purge: {
 		content: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}']
 	},
+	darkMode: false, //or 'media' or 'class'
 	theme: {
 		rotate: {
 			0: '0deg',
--- tailwind.md
+++ tailwind.md
@@ -1,4 +1,4 @@
 ### custom fonts 
 ```html
-<link rel='stylesheet' href='https://use.typekit.net/cub6off.css'>
+<link rel='stylesheet' href='https://use.typekit.net/cub4off.css'>
 ```
